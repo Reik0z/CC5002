a2de07b76a246ed8833fa2d78398b069f8671daf
Reik0 <demian.hott@ug.uchile.cl>
2022-08-31 15:05:05 -0400

    se arregla el select, falta debuggear

--- ejercicio1.js
+++ ejercicio1.js
@@ -9,10 +9,48 @@ function validacion(){
     let nombre = $('#inputNombre');
     let comentarios = $('#inputComentarios');
     let direccion = $('#inputDireccion');
-    // let pizza = document.getElementById('inputPizza')
-
+    let pizza = document.getElementById('inputPizza')
+    
     let telephone = document.getElementById('inputPhone');
     let regex_phone = /^\+\d{11}$/;
+    
+    // ERROR EN TELEFONO
+    if (!regex_phone.test(telephone.value)){
+        errores += '<p> Telefono incorrecto. </p>'
+        validacion = false;
+        document.getElementById('inputPhone').style.borderBlockColor = "red"
+    } else{
+        validacion = true;
+        document.getElementById('inputPhone').style.borderBlockColor = "#ced4da"
+    }
+    
+    // CAMPOS OBLIGATORIOS
+    if (!nombre.val()){
+        errores += '<p> Tiene que ingresar su nombre </p>'
+        validacion = false; 
+        document.getElementById('inputNombre').style.borderColor = "red"
+    }
+
+    if (!telephone.value){
+        errores += '<p> Tiene que ingresar su teléfono. </p>'
+        validacion = false; 
+        document.getElementById('inputPhone').style.borderColor = "red"
+    }
+
+    if (!direccion){
+        errores += '<p> Tiene que ingresar su dirección. </p>'
+        validacion = false; 
+        document.getElementById('inputDireccion').style.borderColor = "red"
+    }
+
+    if (pizza.value == "Italiana" || pizza.value == "Napolitana" || pizza.value == "Hawaiana"){
+        valdacion = true;
+        document.getElementById('inputPizza').style.borderColor = "#ced4da"
+    } else{
+        errores += '<p> Seleccione un tipo de pizza </p>'
+        validacion = false;
+        document.getElementById('inputPizza').style.borderColor = "red"
+    }
 
     // LARGO DE COSAS
     if (nombre.val().length < 5){
@@ -54,31 +92,7 @@ function validacion(){
         document.getElementById('inputDireccion').style.borderColor = "red"
     }
 
-    // ERROR EN TELEFONO
-    if (!regex_phone.test(telephone.value)){
-        errores += '<p> Telefono incorrecto. </p>'
-        validacion = false;
-        document.getElementById('inputPhone').style.borderBlockColor = "red"
-    }
 
-    // CAMPOS OBLIGATORIOS
-    if (!nombre.val()){
-        errores += '<p> Tiene que ingresar su nombre </p>'
-        validacion = false; 
-        document.getElementById('inputNombre').style.borderColor = "red"
-    }
-
-    if (!telephone.value){ // NOSE?
-        errores += '<p> Tiene que ingresar su teléfono. </p>'
-        validacion = false; 
-        document.getElementById('inputPhone').style.borderColor = "red"
-    }
-
-    if (!direccion){
-        errores += '<p> Tiene que ingresar su dirección. </p>'
-        validacion = false; 
-        document.getElementById('inputDireccion').style.borderColor = "red"
-    }
 
     caja_errores.innerHTML = errores;
     return validacion
